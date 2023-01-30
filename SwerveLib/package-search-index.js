95baa4db9dc081db4cd0d5b69754a011c607fa7a
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2023-01-29 21:03:26 -0600

    First commit

---
+++ SwerveLib/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"frc.robot.subsystems.swervedrive.swerve"},{"l":"frc.robot.subsystems.swervedrive.swerve.encoders"},{"l":"frc.robot.subsystems.swervedrive.swerve.kinematics"},{"l":"frc.robot.subsystems.swervedrive.swerve.motors"}]
